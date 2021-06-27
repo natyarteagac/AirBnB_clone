c8bd2d6dd632696b7c22d7d6c759acc07180acbc
klich404 <klich4010@gmail.com>
2021-06-27 11:29:35 -0500

    kwargs implemented

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,19 @@ from datetime import datetime
 
 class BaseModel():
 
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
