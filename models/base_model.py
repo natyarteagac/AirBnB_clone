fd81286683fb9d64c22371a3b43151aeefdfb766
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-27 10:46:47 -0500

    Updating class BaseModel with the methods

--- models/base_model.py
+++ models/base_model.py
@@ -3,27 +3,29 @@
     Defines all common attributes/methods for other classes from BaseModel
 """
 
-import cmd
 from uuid import uuid4
 from datetime import datetime
 
 
-class BaseModel(cmd.Cmd):
+class BaseModel():
 
     def __init__(self):
-        self.id = uuid4()
+        self.id = str(uuid4())
         self.created_at = datetime.now()
-        #self.updated_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({})".format(self.__class__.__name__, self.id)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-    # def do_EOF(self, line):
-        # return True
+    def save(self):
+        self.updated_at = datetime.now()
 
-# if __name__ == '__main__':
-    # HelloWorld().cmdloop()
-
-
-my_object = BaseModel()
-print(my_object.__str__())
+    def to_dict(self):
+        aux = {
+            "__class__": self.__class__.__name__,
+            "updated_at": self.updated_at.isoformat(),
+            "created_at": self.created_at.isoformat()
+        }
+        dict_copy = self.__dict__.copy()
+        dict_copy.update(aux)
+        return dict_copy
