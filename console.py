235edf43a0871388adfe394a7c4823ff0822a5f4
klich404 <klich4010@gmail.com>
2021-06-29 09:17:41 -0500

    command intrepreter implemented

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+contains the entry point of the command interpreter
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """command interpreter"""
+    prompt = '(hbnb)'
+
+    def do_EOF(self, line):
+        "exit the program"
+        return True
+
+    def do_quit(self, line):
+        "Quit command to exit the program"
+        return True
+
+    def emptyline(self):
+        "an empty line + ENTER shouldn’t execute anything"
+        return cmd.Cmd.postloop(self)
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
