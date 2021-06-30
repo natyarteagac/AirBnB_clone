db5fbebb3a3f8a58f18517fefef5c99c78a3000a
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-30 15:41:20 -0500

    Starting with testing

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,3 +2,20 @@
 """
     Testing for BaseModel Class
 """
+import unittest
+import pep8
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Testing BaseModel """
+
+    def test_pep8(self):
+        """ Testing pep8 style """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['base_model.py'])
+        self.assertEqual(result.total_errors, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
