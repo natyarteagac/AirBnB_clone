3bcda227cd7a2b8c9dd73c390df4bed8445af11a
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-30 17:46:20 -0500

    Testing in BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,10 +5,32 @@
 import unittest
 import pep8
 from models.base_model import BaseModel
-import datetime
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
+    """ Testing BaseModel """
+
+    @classmethod
+    def setUpClass(self):
+        """
+        setUp method to create two instances
+        Args:
+            None
+        """
+        self.my_model1 = BaseModel()
+        self.my_model2 = BaseModel()
+
+    @classmethod
+    def tearDownClass(self):
+        """
+        tearDown method
+        Args:
+            None
+        """
+        del self.my_model1
+        del self.my_model2
+
     """Testing BaseModel """
 
     def test_pep8(self):
@@ -19,12 +41,21 @@ class TestBaseModel(unittest.TestCase):
 
     def test_uuid_is_string(self):
         """ Testing if uuid is string """
-        bm = BaseModel()
-        if type(bm.id) is str:
-            self.assertTrue(bm.id)
+        if type(self.my_model1) is str:
+            self.assertTrue(self.my_model1.id)
 
-    def test_time_corresponding_to(self):
-        """ Testing if time correspond to """
+    def test_is_equal(self):
+        """ Testing if time corresponds to """
+
+        self.assertNotEqual(self.my_model1.id, self.my_model2.id)
+
+    def test_atributes(self):
+        """Testing if created_at correspond to my_model"""
+        self.assertTrue(hasattr(self.my_model1, "created_at"))
+        self.assertTrue(hasattr(self.my_model1, "updated_at"))
+        self.assertTrue(hasattr(self.my_model1, "id"))
+        self.assertIsInstance(self.my_model1.created_at, datetime)
+        self.assertIsInstance(self.my_model2.updated_at, datetime)
 
 
 if __name__ == '__main__':
